6f80bbccaf07efbc7e145a62d408ee280e878e81
francisco graya <francisco.graya26@gmail.com>
2025-07-30 09:03:41 -0600

    archivos de configuracion nueva

---
+++ src/main/java/com/ecomers/ecomerskafka/EcomerskafkaApplication.java
@@ -0,0 +1,13 @@
+package com.ecomers.ecomerskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerskafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerskafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomers/ecomerskafka/EcomerskafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomers.ecomerskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerskafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
